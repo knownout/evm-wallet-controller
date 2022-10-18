2e869631679e119cca9373f4293a5f49367d288e
re-knownout <knownout@hotmail.com>
2022-10-18 18:26:07 +0300

    Make network util generic

--- package/utils/network-utils.ts
+++ package/utils/network-utils.ts
@@ -12,7 +12,7 @@ export type TNetworkInfo = {
  * @param {{[p: number]: TNetworkInfo}} networksList available networks list.
  * @return {any}
  */
-export function getNetworksValue (key: keyof TNetworkInfo, networksList: { [key: number]: TNetworkInfo }) {
+export function getNetworksValue<T = TNetworkInfo> (key: keyof T, networksList: { [key: number]: T }) {
     return Object.fromEntries(Object.entries(networksList).map(([ objectKey, value ]) =>
         [ objectKey, value[key] ]
     ));
